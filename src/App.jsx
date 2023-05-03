63d1077f356f9982d4d6912c67e731c3b5cfdd3b
fajar-it <fajar-it@gree.id>
2023-05-03 15:58:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className=" bg-red-300 hover:bg-red-700 md:hover:bg-blue-500 hover:py-5 ease-in-out duration-300">
+        <h1>Gree Indonesia</h1>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
